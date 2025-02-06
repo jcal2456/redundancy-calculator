94b5b08bdbc4dd9ee9e1254c0ea926f8a8682ec9
jcal2456 <jc220083a@students.nulondon.ac.uk>
2025-02-06 14:14:17 +0000

    Update redundancyCalculator.jsV4

--- redundancyCalculator.js
+++ redundancyCalculator.js
@@ -1,29 +1,18 @@
-function calculateRedundancy(
 
-) {
-
-    // Get inputs
-
-    const fullYearsOfService = parseInt(document.getElementById("fullYearsOfService").value);
-
-    const salary = parseInt(document.getElementById("salary").value);
- 
+// Creating module  
+function calculateRedundancy(fullYearsOfService, salary) {
+    let redundancyFormula;
     // Calculate redundancy pay
-    
-    if(fullYearsOfService<2)
-    {   
-        redundancyFormula = "You are not entitled to redundancy pay because you have not worked in the company for at least 2 years "
-        document.getElementById("result").innerHTML = redundancyFormula;
-    }
-    else{
-        redundancyFormula = (fullYearsOfService/2) * (salary/5)
-        if(redundancyFormula> 90000)
-        {
-            redundancyFormula = 90000
+    if(fullYearsOfService < 2) {
+        redundancyFormula = "You are not entitled to redundancy pay because you have not worked in the company for at least 2 years";
+    } else {
+        redundancyFormula = (fullYearsOfService / 2) * (salary / 5);
+        if(redundancyFormula > 90000) {
+            redundancyFormula = 90000;
         }
-        document.getElementById("result").innerHTML = " £ " + redundancyFormula.toFixed(2);
     }
-
+    return redundancyFormula;
 }
 
+// Export module
 module.exports = calculateRedundancy;
