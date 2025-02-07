f75574e33996539d80317a001c4f75abfcc6727f
jcal2456 <jc220083a@students.nulondon.ac.uk>
2025-02-07 18:40:59 +0000

    Update redundancyCalculator.jsV8

--- redundancyCalculator.js
+++ redundancyCalculator.js
@@ -5,14 +5,13 @@ function calculateRedundancy() {
     let salary = document.getElementById("salary").value;
     let redundancyFormula;
 
-        // Convert input values to numbers
+    // Convert input values to numbers
     fullYearsOfService = parseInt(fullYearsOfService);
     salary = parseFloat(salary);
     
     // Calculate redundancy pay
     if(fullYearsOfService < 2) {
         redundancyFormula = "You are not entitled to redundancy pay because you have not worked in the company for at least 2 years";
-        document.getElementById("result").innerHTML = redundancyFormula;
     } else {
         redundancyFormula = (fullYearsOfService / 2) * (salary / 5);
         if(redundancyFormula > 90000) {
@@ -20,6 +19,8 @@ function calculateRedundancy() {
         document.getElementById("result").innerHTML = " £ " + redundancyFormula.toFixed(2);
         }
     }
+    // Display result
+    document.getElementById("result").innerHTML = redundancyFormula;
 }
 
 // Export module
